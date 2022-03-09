e2e9dc5be4b4bc2991b7ef357fbe836d5b242a9f
Rafall9 <rafall9999983@gmail.com>
2022-03-09 20:46:22 +0100

    Zadanie 1

---
+++ lab_1/zadanie_1.py
@@ -0,0 +1,23 @@
+# Napisz pierwszy skrypt,w którym zadeklarujesz po dwie zmienne każdego typu, a następnie wyświetl te zmienne
+
+
+int = 1
+int_2 = 9
+
+string = "string"
+string_2 = "string2"
+
+
+float = 1.21
+float_2 = 4.819
+
+
+print("Pierwsza liczba całkowita: %d" % int)
+print("Druga liczba całkowita: %d" % int_2)
+
+print("Pierwsza liczba zmiennoprzecinkowa: %f" % float)
+print("Druga liczba zmiennoprzecinkowa: %f" % float_2)
+
+print("Pierwszy ciąg znaków: %s" % string)
+print("Drugi ciąg znaków: %s" % string_2)
+
