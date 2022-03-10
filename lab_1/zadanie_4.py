c165ddb9b58dd7343a4a84b1185c98913a222083
Rafall9 <rafall9999983@gmail.com>
2022-03-10 13:20:55 +0100

    zadanie 4

---
+++ lab_1/zadanie_4.py
@@ -0,0 +1,6 @@
+import math
+print("Wynik potęgowania: e^10 =",math.e ** 10 )
+
+print("Wynik działania: (ln(5+sin(8)^2))^(1/6) = ",(math.log(5 + (math.sin(8) ** 2))) ** (1 / 6))
+print("Wynik zaokronglenia w dół: 3,55 =",math.floor(3.55))
+print("Wynik zaokronglenia w górę: 4,8 =" ,math.ceil(4.8))
