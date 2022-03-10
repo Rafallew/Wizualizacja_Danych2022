8d051c7f8b6cbe9f8d15f86d42c412f75dc12c23
Rafall9 <rafall9999983@gmail.com>
2022-03-10 13:51:34 +0100

    zadanie 6

---
+++ lab_1/zadanie_6.py
@@ -0,0 +1,97 @@
+lyrics = """I've been watching you
+A la la la la long
+A la la la la long long li long long long
+Come on
+A la la la la long
+A la la la la long long li long long long
+Hey
+
+Standing across the room, I saw you smile
+I said I wanna talk to you-woo-woo for a little while
+But before I make my move my emotion starts Running wild
+My tongue gets tied and that's no lie
+
+I'm Looking in your eyes
+I'm Looking in your big blue eyes, ooh yeah
+And I've got this to say to you, hey
+
+Girl I want to make you sweat
+Sweat till you can't sweat no more
+And if you cry out
+I'm gonna push it some mo-o-ore
+
+Girl I want to make you sweat
+Sweat till you can't sweat no more
+And if you cry out
+I'm gonna push it, push it push it than more
+
+A la la la la long
+A la la la la long long li long long long
+Come on
+A la la la la long
+A la la la la long long li long long long
+Ooh
+
+So I say to myself if she loves me or not
+But the dread I know that love is his to hers
+A with a little bit of this and a little bit of that
+The lyrics goes on the attack
+My tongue gets tied, and that's no lie
+
+I'm Looking in your eyes
+I'm Looking in your big blue eyes, ooh girl
+And I've got this to say to you, hey
+
+Girl I want to make you sweat
+Sweat till you can't sweat no more
+And if you cry out
+I'm gonna push it some mo-o-ore
+
+Girl I want to make you sweat
+Sweat till you can't sweat no more
+And if you cry out
+I'm gonna push it, push it than more.
+
+A la la la la long
+A la la la la long long li long long long
+Ooh yeah
+A la la la la long
+A la la la la long long li long long long
+One more time
+A la la la la long
+A la la la la long long li long long long
+Sing it
+A la la la la long
+A la la la la long long li long long long
+Hey
+
+Woo woo woo woo-ee, yeah
+
+Eyes, I'm looking in your big blue eyes, ooh yeah
+And I've got this to say to you, hey
+
+Girl I want to make you sweat
+Sweat till you can't sweat no more
+And if you cry out
+I'm gonna push it some mo-o-ore
+
+Girl I want to make you sweat
+Sweat till you can't sweat no more
+And if you cry out
+I'm gonna push it, push it, push it than more.
+
+A la la la la long
+A la la la la long long li long long long
+Yeah
+A la la la la long
+A la la la la long long li long long long
+Push it push it some more
+A la la la la long
+A la la la la long long li long long long
+All right
+A la la la la long
+A la la la la long long li long long long
+Push it push it some more
+
+"""
+print("W piosence 'A lalala long - Bob Marley' słowo 'la' występuje: ",lyrics.lower().count('la'),"razy.")
