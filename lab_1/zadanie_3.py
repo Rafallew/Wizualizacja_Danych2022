72c7906bd0cb71ee4257a2a2b81723edcbc775d7
Rafall9 <rafall9999983@gmail.com>
2022-03-10 12:48:41 +0100

    zadaie 3

---
+++ lab_1/zadanie_3.py
@@ -0,0 +1,15 @@
+liczba = 1.0
+print("Stan początkowy: ", liczba)
+liczba += 3
+print("Po dodaniu: 3 = ",  liczba)
+liczba -= 2
+print("Po odjęciu: 2 = ", liczba)
+liczba *= 3
+print("Po przemnożeniu przez: 3 = ", liczba)
+liczba /= 2
+print("Po podzieleniu przez: 2 = ", liczba)
+liczba **= 2
+print("Po podniesieniu do potęgi: 2 = ", liczba)
+liczba %= 6
+print("Po wykonaniu modulo przez: 6 = ", liczba)
+
