1875a6c8286763bd483608f90118c52d6331679c
Rafall9 <rafall9999983@gmail.com>
2022-03-10 16:44:38 +0100

    zadanie 9

---
+++ lab_1/zadanie_9.py
@@ -0,0 +1,9 @@
+
+imie = "Rafał"
+liczba_φ = 1.6180339887
+numer = 435
+
+
+print("Nazywam sie", imie)
+print("Liczba pi to w przybliżeniu %.2f" % liczba_φ)
+print(numer,"w systemie szesnastkowym to: ",hex(numer))
