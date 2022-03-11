2dd9df6ed0aceb3e6281cd3993e31f0328103736
Rafall9 <rafall9999983@gmail.com>
2022-03-11 12:13:07 +0100

    zadanie4

---
+++ lab_2/zadanie_4.py
@@ -0,0 +1,2 @@
+zdanie_uzytkownika = input("Wpisz zdanie, jeżeli chcesz policzyc ile razy występuje litera 'a':")
+print("W tym zdaniu litera 'a' występuje: ",zdanie_uzytkownika.lower().count('a'),"razy.")
