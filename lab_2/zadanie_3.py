eb0cd1ab73c05ff3285603307a4656b0a50dba67
Rafall9 <rafall9999983@gmail.com>
2022-03-11 12:02:40 +0100

    zadanie3

---
+++ lab_2/zadanie_3.py
@@ -0,0 +1,9 @@
+ulubione_gry = {
+    'Gry przygodowe': 'Minecraft',
+    'Gry zręcznściowe': 'Worms',
+    'Gry fabularne': 'Wiedźmin 3: Dziki Gon',
+    'Gry sportowe': 'FIFA 22'
+}
+
+print(ulubione_gry)
+print("Ilość elementów w słowniku: ", len(ulubione_gry.keys()))
