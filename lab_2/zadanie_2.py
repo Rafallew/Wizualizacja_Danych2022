82a294671a0663f4c35c0dcbc9e53a1b337de4fe
Rafall9 <rafall9999983@gmail.com>
2022-03-11 10:44:47 +0100

    zadanie2

---
+++ lab_2/zadanie_2.py
@@ -0,0 +1,14 @@
+abbreviations = {
+    'al.': 'aleja',
+    'cd.': 'ciąg dalszy',
+    'cdn.': 'ciąg dalszy nastąpi',
+    'CV': 'curriculum vitae',
+    'dr': 'doktor',
+    'itd.': 'i tak dalej',
+    'itp.': 'i tym podobne',
+    'lek.': 'lekarz',
+    'lic.': 'liencjat',
+    'mgr': 'magister',
+    'NIP': 'Numer Identyfikacji Podatkowej'
+}
+print(abbreviations)
