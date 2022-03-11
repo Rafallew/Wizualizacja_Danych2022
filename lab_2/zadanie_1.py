f4913daca776f7c08208223aa43984b6fdac1f19
Rafall9 <rafall9999983@gmail.com>
2022-03-11 10:30:02 +0100

    zadanie1

---
+++ lab_2/zadanie_1.py
@@ -0,0 +1,10 @@
+sport = ['kolarstwo', 'koszykówka', 'rugby']
+print("Lista ulubionych sportów: ", sport)
+
+sport.reverse()
+print("Lista ulubionych sportów po zmianie kolejności: ", sport)
+
+sport.append('szermierka')
+sport.append('piłka nożna')
+sport.append('kajakarstwo')
+print("Lista ulubionych sportów po zmianie kolejności i dodaniu mniej lubianych sportów: ", sport)
