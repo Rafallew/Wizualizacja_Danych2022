cf1b32c32e181f66d84fcaebc55b00aeec0fcded
Rafall9 <rafall9999983@gmail.com>
2022-03-23 21:06:11 +0100

    zadanie 2

---
+++ lab_3/zadanie_2.py
@@ -0,0 +1,7 @@
+from random import randint as random
+
+lista1 = [random(1, 100) for i in range(0, 10)]
+print("10 wygenerowanych losowo elementów: ", lista1)
+
+lista2 = [x for x in lista1 if x % 2 == 0]
+print("Tylko parzyste elementy z powyższych: ", lista2)
